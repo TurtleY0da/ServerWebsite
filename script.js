d645973abbd9f6c88f10c3fbf90cb15435de03db
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-13 21:51:00 -0600

    Fifth, and current final commit of SMP Website

---
+++ script.js
@@ -0,0 +1,8 @@
+// Madlibs by Timothy Veugelers
+
+// Give Button Event Listener
+document.getElementById("rulesButton").addEventListener("click", gotoRules);
+
+function gotoRules() {
+    location.replace("/rules.html")
+}
